f0584e9e5e4c38c251432ddf5ed7a595681f0f28
Jeremy <jeremy.laumon@gmail.com>
2024-01-25 21:14:03 +0100

    Added Timer helper struct

--- src/Ticks.h
+++ src/Ticks.h
@@ -7,3 +7,11 @@ int64  gTicksToNanoSeconds(int64 inTicks);
 double gTicksToSeconds(int64 inTicks);
 
 const int64 gProcessStartTicks = gGetTickCount();
+
+struct Timer : NoCopy
+{
+	Timer() { Reset(); }
+	void  Reset() { mTicks = gGetTickCount(); }
+	int64 GetTicks() const { return gGetTickCount() - mTicks; }
+	int64 mTicks = 0;
+};
