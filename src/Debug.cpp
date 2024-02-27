71fca331b7577062dff2bf9cb5490dad545c9fae
Jeremy <jeremy.laumon@gmail.com>
2024-02-27 20:49:04 +0100

    Removed trailing EOL in GetLastErrorString

--- src/Debug.cpp
+++ src/Debug.cpp
@@ -17,12 +17,19 @@ String GetLastErrorString()
 	if (error == ERROR_SUCCESS)
 		return {};
 
-	// TODO: there is usually a \r\n at the end, remove it!
 	char  buffer[512];
 	DWORD buffer_len = FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, error,
 									  MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), buffer, sizeof(buffer) - 1, nullptr);
 	// Make sure there is a null terminator (FormatMessage does not always add one).
 	buffer[buffer_len] = 0;
+
+	// There's usually an EOL at the end, remove it.
+	while (gEndsWith({ buffer, buffer_len }, "\n") || gEndsWith({ buffer, buffer_len }, "\r"))
+	{
+		buffer_len--;
+		buffer[buffer_len] = 0;
+	}
+
 	return buffer;
 }
 
