3ee0121dce7240eb1c6b41cca179d9b8a16b38b5
Jeremy <jeremy.laumon@gmail.com>
2024-03-28 20:50:40 +0100

    Finally a Too Many Cooks reference

--- src/UI.cpp
+++ src/UI.cpp
@@ -1021,6 +1021,7 @@ void gDrawStatusBar()
 			ICON_FK_CUTLERY" Let's cook! LET'S COOK!",
 			ICON_FK_CUTLERY" A fort wasn't cooked in a day. It took 0.4 seconds.",
 			ICON_FK_CUTLERY" Are you going to change a file today?",
+			ICON_FK_CUTLERY" Too many cooks? Too many Cooks!",
 		};
 		static int message_choice = gRand32() % gElemCount(messages);
 		ImGui::TextUnformatted(messages[message_choice]);
